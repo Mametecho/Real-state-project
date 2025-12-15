2d1523152f22c1e03dfb356b4327c6e177da2d44
Mametecho <mohammed9161.ma.com>
2025-12-15 10:46:11 +0300

    connect to Daatabase using mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from "express";
+import connectDB from "./config/db.js";
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
